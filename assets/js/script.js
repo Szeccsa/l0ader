da46cc034ebe0ee2e4aceac29cdcc1ff4d2c2ef7
Kevin Király <95046202+Szeccsa@users.noreply.github.com>
2024-07-13 09:08:47 +0200

    v4.0

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+const sentences = [
+    "Hang tight! We're loading the page.",
+    "Loading, please be patient...",
+    "Just a moment, we're almost ready."
+];
+
+function showRandomSentence() {
+    const randomIndex = Math.floor(Math.random() * sentences.length);
+    const randomSentence = sentences[randomIndex];
+    document.getElementById('loading-message').innerText = randomSentence;
+}
+
+showRandomSentence();
+
+(function () {
+    window.onload = function () {
+        const preloader = document.querySelector('.page-loading');
+        preloader.classList.remove('active');
+        setTimeout(function () {
+            preloader.remove();
+        }, 1500);
+    }
+})();
